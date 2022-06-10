d27a8d6ae26a4c1aff1c4c3bc475a78d631d9bf8
aayushsrivastava0 <aayush.srivastava@knoldus.com>
2022-06-10 07:10:11 +0530

    Resume extraction to json

--- ResumeNLP/Data_Extraction/src/Test_Data_morpher.py
+++ ResumeNLP/Data_Extraction/src/Test_Data_morpher.py
@@ -1,21 +1,35 @@
- ##
- ## Entity Extraction using Pyreparser an open source library
+## Entity Extraction using Pyreparser an open source library
 
 from pyresparser import ResumeParser
 import json
 import spacy
 import nltk
-
-
-path_to_pdf = r'/home/aayush/PycharmProjects/ResumeNLP/Data/Input_data/Navnath Harihar_Data Scientist.pdf'
-
-data = ResumeParser(path_to_pdf).get_extracted_data()   #entity extraction saving the results in a dict
-#print(data)
+import os
 
 
 
-with open("sample.json", "w") as outfile:               #json conversion
-    json.dump(data, outfile)
+
+# Resume Entity extraction
 
 
+INPUT_DIRECTORY = r'/home/aayush/PycharmProjects/ResumeNLP/Data/Input_data'
+OUTPUT_DIRECTORY = r'/home/aayush/PycharmProjects/ResumeNLP/Data/Extracted_data/'
+extension = (".pdf", ".docx")
 
+def resume_extraction(inp_dirc,ext_direc):
+    try:
+
+        for root, dirs, files in os.walk(inp_dirc):
+            for dir in dirs:
+                filepath = os.path.join(inp_dirc, dir)
+                for files in os.listdir(filepath):
+                    if files.endswith(extension):
+                        data = ResumeParser(os.path.join(filepath, files)).get_extracted_data()   #Getting the extracted data in a dict using pyreparser
+                        out_file = open(os.path.join(ext_direc, files + ".json"), "w")
+                        json.dump(data, out_file, indent=4, sort_keys=False)
+                        out_file.close()
+    except Exception as e:
+        print(e)
+
+
+resume_extraction(INPUT_DIRECTORY,OUTPUT_DIRECTORY)
